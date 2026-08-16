a9df3f161e5816efafe36598b0419ab596baabc9
Caio <caiokaea@gmail.com>
2026-08-16 12:38:09 -0300

    feat: exercise 03 from indroduction added

---
+++ introducao_a_programacao_orientada_a_objetos/exercise03/Program.java
@@ -0,0 +1,39 @@
+package exercise03;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        System.out.println("Digite os dados do aluno");
+
+        System.out.println("Nome: ");
+        student.name = sc.nextLine();
+
+        System.out.println();
+        System.out.println("Primeira nota: ");
+        student.firstGrade = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Segunda nota: ");
+        student.secondGrade = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Terceira nota: ");
+        student.thirdGrade = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Aluno: " + student.name);
+        System.out.println("Nota final: " + student.finalGrade());
+
+        System.out.println();
+        student.isApproved();
+
+        sc.close();
+    }
+}
---
+++ introducao_a_programacao_orientada_a_objetos/exercise03/Student.java
@@ -0,0 +1,20 @@
+package exercise03;
+
+public class Student {
+    public String name;
+    public double firstGrade;
+    public double secondGrade;
+    public double thirdGrade;
+
+    public double finalGrade() {
+        return (firstGrade + secondGrade + thirdGrade);
+    }
+
+    public void isApproved() {
+        if (finalGrade() >= 60.0) {
+            System.out.println("APROVADO!");
+        } else {
+            System.out.println("REPROVADO, faltou " + (60.0 - finalGrade()) + " pontos para ser aprovado!");
+        }
+    }
+}
